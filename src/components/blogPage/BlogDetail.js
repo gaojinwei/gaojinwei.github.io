1e6f5f5dab55eee20ea58e6bffcb3fe0d8ae6a8f
gaojinwei <1194529437@qq.com>
2018-08-15 21:56:23 +0800

    图片点击放大功能

--- src/components/blogPage/BlogDetail.js
+++ src/components/blogPage/BlogDetail.js
@@ -23,8 +23,17 @@ class BlogDetail extends React.Component {
 		super(props);
 		this.state = {
 			article: null,
-			loading: true
+			loading: true,
+			showBigImg: false,
+			bigImgSrc: ""
 		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e){
+		this.setState({
+	        showBigImg: false
+	    });
 	}
 
 	componentDidMount() {
@@ -37,18 +46,55 @@ class BlogDetail extends React.Component {
 	        	loading: false
 	     	});
 	      });
+
+	    const blogDiv = document.getElementById("blogDetail");
+	    /*blogDiv.onclick = function(event) {
+		  let target = event.target; // where was the click?
+
+		  if (target.tagName != 'IMG') return; // not on TD? Then we're not interested
+
+		  alert(target.src); // highlight it
+		};*/
+
+		//之所以改为箭头函数，是因为要this.setState，关键在于这个this
+		blogDiv.onclick = event => {
+		  let target = event.target; // where was the click?
+
+		  if (target.tagName != 'IMG') return; // not on TD? Then we're not interested
+
+		  this.setState({
+	          showBigImg: true,
+			  bigImgSrc: target.src
+	      });
+
+		};
+
 	}
 
 	render(){
 		const isLoading = this.state.loading;
+		const showBigImg = this.state.showBigImg;
+		const bigImgSrc = this.state.bigImgSrc;
+
+		let bigImg;
+		if (showBigImg) {
+			bigImg =  (
+				<div className="previewImg flexDiv" onClick={this.handleClick}>
+	    			<img className="bigImg" src={bigImgSrc} />
+	    		</div>
+			)				
+		}
 		return (
-	    	<div className="blogWrapper">
+	    	<div className="blogWrapper" id="blogDetail">
 	    		{
 	    			isLoading ? (
 	    				<Loading />
 	    		 	) : (
-	    		 	 	<div className="article-detail" dangerouslySetInnerHTML={{ __html: marked(this.state.article.body) }}>
-			    		</div>
+	    		 		<React.Fragment>
+		    		 	 	<div className="article-detail" dangerouslySetInnerHTML={{ __html: marked(this.state.article.body) }}>
+				    		</div>
+				    		{bigImg}
+				    	</React.Fragment>
 		    		)
 	    		}
 	    	</div>
